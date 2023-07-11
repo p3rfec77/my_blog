8b5d3d52068494c9956b6394cfc3ce2b6acf1fd2
Kirill Golygin <kirik3007@gmail.com>
2023-07-11 20:42:49 +0300

    cover section

--- components/Cover/index.jsx
+++ components/Cover/index.jsx
@@ -3,8 +3,15 @@ import React from "react";
 import cl from "classnames";
 import styles from "./index.module.scss";
 
-const Cover = ({ classname }) => {
-  return <div>Cover</div>;
+const Cover = ({ classname, title }) => {
+  return (
+    <div className={cl(classname, styles.cover)}>
+      <h1
+        className={styles.coverTitle}
+        dangerouslySetInnerHTML={{ __html: title }}
+      />
+    </div>
+  );
 };
 
 export default Cover;
--- components/SocialNetworks/index.jsx
+++ components/SocialNetworks/index.jsx
@@ -1,10 +1,54 @@
 import React from "react";
 
+import {
+  AiFillYoutube,
+  AiFillGithub,
+  AiFillTwitterCircle,
+  AiFillInstagram,
+} from "react-icons/ai";
+
+import ScreenEgg from "../ScreenEgg";
+
 import cl from "classnames";
 import styles from "./index.module.scss";
 
-const SocialNetworks = ({ classname }) => {
-  return <div>SocialNetworks</div>;
+const SocialNetworks = [
+  {
+    id: 1,
+    link: "https://www.youtube.com/",
+    icon: AiFillYoutube,
+  },
+  {
+    id: 2,
+    link: "https://github.com/p3rfec77",
+    icon: AiFillGithub,
+  },
+  {
+    id: 3,
+    link: "https://instagram.com/golygin_k.a?igshid=MmIzYWVlNDQ5Yg==",
+    icon: AiFillInstagram,
+  },
+  {
+    id: 4,
+    link: "https://twitter.com/",
+    icon: AiFillTwitterCircle,
+  },
+];
+
+const SocialNetwork = ({ classname }) => {
+  return (
+    <ScreenEgg>
+      <ul className={cl(classname, styles.list)}>
+        {SocialNetworks.map(({ id, link, icon }) => (
+          <li key={id} className={styles.listItem}>
+            <a href={link} target="_blank" className={styles.listLink}>
+              {icon({ size: 50, color: "black" })}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </ScreenEgg>
+  );
 };
 
-export default SocialNetworks;
+export default SocialNetwork;
